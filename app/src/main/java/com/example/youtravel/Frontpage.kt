be33cc61f3944af78de7d3a830896b15548f366f
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-06-04 14:45:12 +0100

    some changes

--- app/src/main/java/com/example/youtravel/Frontpage.kt
+++ app/src/main/java/com/example/youtravel/Frontpage.kt
@@ -6,15 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class introsliders : AppCompatActivity() {
+class Frontpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_introsliders)
+        setContentView(R.layout.activity_frontpage)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
