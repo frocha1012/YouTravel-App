8a643589fecdb1382e53f4d53f35f391ea1d7591
Fernando Rocha <frocha2910@gmail.com>
2024-06-30 22:28:18 +0100

    Dark mode Registo, Login, IntroSliders, Frontpage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("com.android.volley:volley:1.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
     implementation("com.google.maps.android:places-ktx:2.0.0")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
